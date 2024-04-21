7de4825f1ef46b0f4612243f8371754970965183
ADIGA612 <h97abaza4pro@gmail.com>
2024-04-21 19:36:28 +0300

    feat: Impelement timestamp api service

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  var date_param = req.params.date;
+  var date = new Date();
+  if (date_param)
+    date = Number.isInteger(parseInt(date_param))
+      ? new Date(parseInt(date_param))
+      : new Date(date_param);
+  if (date == "Invalid Date") res.json({ error: "Invalid Date" });
+  else res.json({ unix: date.getTime(), utc: date.toGMTString() });
 });
 
 
